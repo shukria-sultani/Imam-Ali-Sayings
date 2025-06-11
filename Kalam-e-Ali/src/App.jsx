cd9af5d95b72ec702d330c31430a22d3d5861dd8
Shukria <shukriasul5@gmail.com>
2025-06-11 09:28:51 +0430

    Added the CategoryButtons component and applied styling to it.

--- Kalam-e-Ali/src/App.jsx
+++ Kalam-e-Ali/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Hero from './Components/Hero'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CategoryButtons from './Components/CategoryButtons';
 
 function App() {
   return(
     <>
     <Hero></Hero>
+    <CategoryButtons></CategoryButtons>
     
     </>
   )
---
+++ Kalam-e-Ali/src/Components/CategoryButtons.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export default function CategoryButtons() {
+  const inactiveStyle = {
+    backgroundColor: "#FFC236",
+    boxShadow: " 4px 4px 6px rgba(58, 187, 82, 0.7)",
+    border: "none",
+    outline: "one",
+    fontSize: "x-large",
+  };
+  const activeStyle = {
+    backgroundColor: "#40C057",
+    boxShadow: "none",
+    border: "none",
+    outline: "one",
+    fontSize: "x-large",
+    transition: "2s"
+  };
+
+  let [activeCategory, setActiveCategory] = useState(null);
+
+  function handleCategroyClick(category) {
+    setActiveCategory(category);
+  }
+
+  return (
+    <>
+      <div className="container category-buttons mt-5  pt-5 d-flex flex-row gap-2 ">
+        <button
+          className="btn p-2 ps-lg-5  ms-sm-3 pe-lg-5 ps-sm-3 pe-sm-5 btn-lg "
+          onClick={() => handleCategroyClick("نامه ها")}
+          style={activeCategory === "نامه ها" ? activeStyle : inactiveStyle}
+        >
+          نامه ها
+        </button>
+        <button
+          className="btn p-2 ps-lg-5  ms-sm-3 pe-lg-5 ps-sm-3 pe-sm-5 btn-lg "
+          onClick={() => handleCategroyClick("خطبه ها")}
+          style={activeCategory === "خطبه ها" ? activeStyle : inactiveStyle}
+        >
+          خطبه ها{" "}
+        </button>
+        <button
+          className="btn p-2 ps-lg-5 ms-sm-3  pe-lg-5 ps-sm-3 pe-sm-5 btn-lg "
+          onClick={() =>handleCategroyClick ("حکمت ها")}
+          style={activeCategory === "حکمت ها" ? activeStyle : inactiveStyle}
+        >
+          حکمت ها
+        </button>
+      </div>
+    </>
+  );
+}
