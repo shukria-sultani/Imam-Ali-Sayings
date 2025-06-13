d7222347d66a500c3861baaa73bb61bb58cfa8fe
Shukria <shukriasul5@gmail.com>
2025-06-13 15:29:34 +0430

    Added the Content component to display the content  and applied styling to it .

--- Kalam-e-Ali/src/App.jsx
+++ Kalam-e-Ali/src/App.jsx
@@ -3,13 +3,17 @@ import Hero from './Components/Hero'
 import 'bootstrap/dist/css/bootstrap.min.css';
     import 'bootstrap/dist/js/bootstrap.min.js';
 import CategoryButtons from './Components/CategoryButtons';
-
+import ContentNavigator from './Components/ContentNavigator';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 function App() {
   return(
     <>
     <Hero></Hero>
     <CategoryButtons></CategoryButtons>
-    
+    <ContentNavigator></ContentNavigator>
+    <Content></Content>
+    <Footer></Footer>
     </>
   )
 
