44e4ed13d996e77e1ca6bf1b7485c26575913615
Shukria <shukriasul5@gmail.com>
2025-06-13 15:29:56 +0430

    Added the Footer component.

---
+++ Kalam-e-Ali/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="container pt-5 footer d-flex flex-column align-items-center justify-content-center">
+        <h1>کلام علی</h1>
+        <h2>کانال واتساپ "فرج هست راه نجات ما"</h2>
+        <h2>   اللهم عجل لولیک الفرج 🤲 </h2>
+      </div>
+    </>
+  );
+}
