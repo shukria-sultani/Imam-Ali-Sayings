4cd29a7d1146f0e6d2d65c817b4b982e173456d4
Shukria <shukriasul5@gmail.com>
2025-06-13 17:06:36 +0430

    Fixed the order of the buttons .

--- Kalam-e-Ali/src/Components/CategoryButtons.jsx
+++ Kalam-e-Ali/src/Components/CategoryButtons.jsx
@@ -29,19 +29,20 @@ export default function CategoryButtons() {
       <div className="container w-100 mt-5 pt-5">
         <div className="d-flex  w-100 flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
           <div className="d-flex flex-row gap-2 flex-wrap justify-content-center mb-3 mb-lg-0"> 
-            <button
-              className="btn btn-lg p-2 ps-3 pe-3 ps-md-4 pe-md-4"
-              onClick={() => handleCategoryClick("نامه ها")}
-              style={activeCategory === "نامه ها" ? activeStyle : inactiveStyle}
-            >
-              نامه ها
-            </button>
+           
             <button
               className="btn btn-lg ps-3 pe-3 ps-md-4 pe-md-4"
               onClick={() => handleCategoryClick("خطبه ها")}
               style={activeCategory === "خطبه ها" ? activeStyle : inactiveStyle}
             >
               خطبه ها{" "}
+            </button>
+             <button
+              className="btn btn-lg p-2 ps-3 pe-3 ps-md-4 pe-md-4"
+              onClick={() => handleCategoryClick("نامه ها")}
+              style={activeCategory === "نامه ها" ? activeStyle : inactiveStyle}
+            >
+              نامه ها
             </button>
             <button
               className="btn btn-lg ps-3 pe-3 ps-md-4 pe-md-4"
