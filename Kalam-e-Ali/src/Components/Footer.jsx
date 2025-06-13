9d19c1711907df3512f81d7db42dc458ce85bfc7
Shukria <shukriasul5@gmail.com>
2025-06-13 16:00:59 +0430

    Added the WhatsApp channel's link to the footer.

--- Kalam-e-Ali/src/Components/Footer.jsx
+++ Kalam-e-Ali/src/Components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default function Footer() {
+
   return (
     <>
       <div className="container pt-5 footer d-flex flex-column align-items-center justify-content-center">
         <h1>کلام علی</h1>
-        <h2>کانال واتساپ "فرج هست راه نجات ما"</h2>
-        <h2>   اللهم عجل لولیک الفرج 🤲 </h2>
+        <h2><a href="https://whatsapp.com/channel/0029Vb3XJVr3gvWgnM8Dh31g" target="blank" className="text-dark" > واتساپ کانال <span style={{color: "#40c057", fontFamily: "Mj_Ramollah"}}>"فرج هست راه نجات ما"</span>  </a></h2>
+        <h2 className="pt-2">   اللهم عجل لولیک الفرج 🤲 </h2>
       </div>
     </>
   );
