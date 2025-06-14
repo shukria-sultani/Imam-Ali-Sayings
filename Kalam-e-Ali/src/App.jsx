358805e0ce73b1000a6331b9db50dace6f5b0bd2
Shukria <shukriasul5@gmail.com>
2025-06-14 09:40:03 +0430

    Set up core content management and interactivity
    This commit introduces the foundational logic for dynamic content in the Kalam-e-Ali app.
    - Implemented state for active category, content list, and current item index.
    - Added logic for switching categories (خطبه ها, نامه ها, حکمت ها).
    - Developed navigation functions (next/back) for content items.
    - Prepared data for dynamic rendering in child components.
    -Added the data files for the app content.

--- Kalam-e-Ali/src/App.jsx
+++ Kalam-e-Ali/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Hero from './Components/Hero'
 import 'bootstrap/dist/css/bootstrap.min.css';
     import 'bootstrap/dist/js/bootstrap.min.js';
+
+// components
 import CategoryButtons from './Components/CategoryButtons';
 import ContentNavigator from './Components/ContentNavigator';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
+
+// data
+import namah from "./data/namah";
+import khutbah from "./data/khutbah";
+import hekmat from "./data/hekmat";
+
+
+
+
 function App() {
+
+  const [activeCategory, setActiveCategory] = useState(null);
+  const [currentContentList, setCurrentContentList] = useState([]);
+  const[currentContentIndex, setCurrentContentIndex] = useState(0)
+
+  useEffect(() => {
+    const defaultCategory = "خطبه ها";
+    setActiveCategory(defaultCategory);
+    setCurrentContentList(khutbah);  
+  }, []);
+
+
   return(
     <>
     <Hero></Hero>
