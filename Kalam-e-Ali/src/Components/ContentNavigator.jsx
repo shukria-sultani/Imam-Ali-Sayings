943c7ef914c5f0da314f6232824dd50c23138de6
Shukria <shukriasul5@gmail.com>
2025-06-14 16:31:58 +0430

    Fixed the bag in the next and prev buttons in the ContentNavigator component.

--- Kalam-e-Ali/src/Components/ContentNavigator.jsx
+++ Kalam-e-Ali/src/Components/ContentNavigator.jsx
@@ -6,13 +6,13 @@ export default function ContentNavigator({currentNumber, totalNumber, onNext, on
   
       <div className="container d-flex flex-row justify-content-between mt-5">
         <div className="countKalam  pt-3">
-          <span className='display-6'>{currentNumber} از </span> <span className='display-6'>{totalNumber}</span>
+          <span className='display-6'>{currentNumber + 1} از </span> <span className='display-6'>{totalNumber}</span>
         </div>
 
         <div className="navigator">
           <div className=''>
-            <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/40C057/circled-right-2.png" alt="circled-right-2" className='m-3' style={{cursor: "pointer"}} onClick={()=> onNext()}/>
-            <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/40C057/circled-left-2.png" alt="circled-left-2" style={{cursor: "pointer"}} onClick={()=> onBack()}/>
+            <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/40C057/circled-right-2.png" alt="circled-right-2" className='m-3' style={{cursor: "pointer"}} onClick={()=> onBack()}/>
+            <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/40C057/circled-left-2.png" alt="circled-left-2" style={{cursor: "pointer"}} onClick={()=> onNext()}/>
           </div>
         </div>
       </div>
