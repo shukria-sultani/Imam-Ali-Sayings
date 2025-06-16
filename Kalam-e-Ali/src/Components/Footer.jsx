f04cc5c61d8d0106cc529649de033815726993d3
Shukria <shukriasul5@gmail.com>
2025-06-16 08:49:00 +0430

    Fixed the mistake in the order of words in the footer.

--- Kalam-e-Ali/src/Components/Footer.jsx
+++ Kalam-e-Ali/src/Components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <>
       <div className="container pt-5 footer d-flex flex-column align-items-center justify-content-center">
         <h1>کلام علی</h1>
-        <h2><a href="https://whatsapp.com/channel/0029Vb3XJVr3gvWgnM8Dh31g" target="blank" className="text-dark" > واتساپ کانال <span style={{color: "#40c057", fontFamily: "Mj_Ramollah"}}>"فرج هست راه نجات ما"</span>  </a></h2>
+        <h2><a href="https://whatsapp.com/channel/0029Vb3XJVr3gvWgnM8Dh31g" target="blank" className="text-dark" > کانال واتساپ <span style={{color: "#40c057", fontFamily: "Mj_Ramollah"}}>"فرج هست راه نجات ما"</span>  </a></h2>
         <h2 className="pt-2">   اللهم عجل لولیک الفرج 🤲 </h2>
       </div>
     </>
